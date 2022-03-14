7a9a66628e8076d72a0ebc2d91df777f880a30fa
영업_구예지 <clclvl1234@gmail.com>
2022-03-14 13:44:39 +0900

    test수정

--- GitJava/src/SalesTest1.java
+++ GitJava/src/SalesTest1.java
@@ -2,12 +2,8 @@
 public class SalesTest1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		System.out.println("이렇게하면 되는건가용?");
-=======
-		System.out.println("test");
->>>>>>> branch 'main' of https://github.com/axia911/gdj43.git
 	}
 
 }
