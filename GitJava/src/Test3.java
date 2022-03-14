99ffe6152c01a0eab57f150f11f3bb4bda180796
고객지원_김민호 <rla789753@naver.com>
2022-03-14 14:02:48 +0900

    ㅇㅇ

--- GitJava/src/Test3.java
+++ GitJava/src/Test3.java
@@ -3,7 +3,7 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test 333");
+		System.out.println("test 332");
 	}
 
 }
--- GitJava/src/TestKmh.java
+++ GitJava/src/TestKmh.java
@@ -2,7 +2,7 @@
 public class TestKmh {
 
 	public static void main(String[] args) {
-		System.out.println("Test중입니다1");
+		System.out.println("Test중입니다2");
 
 	}
 
