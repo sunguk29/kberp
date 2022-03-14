584cafa60d0a32c39182966a39042ddafa5168b9
최현우 <gusdn3048@naver.com>
2022-03-14 13:47:50 +0900

    5

--- GitJava/src/Test1.java
+++ GitJava/src/Test1.java
@@ -3,12 +3,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello112");
-		System.out.println("Hello3");
-		
-		System.out.println("Hi");
-		System.out.println("Hello2");
-		
-		System.out.println("test입니다.");
+		System.out.println("Hello5");
 	}
 
 }
