4687369aeb4e5f1c22019c98a2914f4e540fae83
영업_김민재 <poodle7192@gmail.com>
2022-03-14 16:06:27 +0900

    테스

--- GitJava/src/SalesTest1.java
+++ GitJava/src/SalesTest1.java
@@ -16,6 +16,7 @@ public class SalesTest1 {
 		System.out.println("김민재22");
 		System.out.println("김민재33");
 		System.out.println("김민재44");
+		System.out.println("김민재55");
 		
 		System.out.println("testtesttesttesttesttesttesttesttest");
 		System.out.println("lasttest2");
