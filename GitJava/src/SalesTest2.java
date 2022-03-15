ec86c37e88fa5cea8ddb908e1c1b8f699725c5d5
영업_유은정 <eundang41@naver.com>
2022-03-15 18:59:13 +0900

    test

--- GitJava/src/SalesTest2.java
+++ GitJava/src/SalesTest2.java
@@ -10,6 +10,7 @@ public class SalesTest2 {
 		System.out.println("merge 헷갈림3");
 		System.out.println("merge 헷갈림3");
 		System.out.println("mac 에바임");
+		System.out.println("mac 왜 계속 로그인하라하는데");
 	}
 
 }
