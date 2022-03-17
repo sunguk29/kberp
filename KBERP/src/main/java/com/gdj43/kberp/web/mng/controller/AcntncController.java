8e801cba7cda5286c4ecc38213e9900c062e0af2
백종훈 <hun3072@gmail.com>
2022-03-17 17:56:15 +0900

    지출결의서 연결

--- KBERP/src/main/java/com/gdj43/kberp/web/mng/controller/AcntncController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/mng/controller/AcntncController.java
@@ -1,5 +1,28 @@
 package com.gdj43.kberp.web.mng.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gdj43.kberp.common.service.IPagingService;
+import com.gdj43.kberp.web.common.service.ICommonService;
+
+@Controller
 public class AcntncController {
+	
+	@Autowired
+	public ICommonService iCommonService;
+	
+	@Autowired
+	public IPagingService iPagingService;
+	
+	@RequestMapping(value = "/expnsRsltn")
+	public ModelAndView expnsRsltn(ModelAndView mav) {
+		
+		mav.setViewName("mng/expnsRsltn");
+		
+		return mav;
+	}
 
 }
