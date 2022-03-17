16cc2120842b14db83f9c7424f61a7eb2ba0a659
영업_조경은 <universe2261877@gmail.com>
2022-03-17 17:41:33 +0900

    영업관리 기본작업

--- KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesMng.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesMng.java
@@ -1,5 +1,17 @@
 package com.gdj43.kberp.web.sales.controller;
 
-public class SalesMng {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.gdj43.kberp.web.common.service.ICommonService;
+import com.gdj43.kberp.web.sales.service.ISalesMngService;
+
+@Controller
+public class SalesMng {
+	@Autowired
+	public ICommonService iCommonService;
+	
+	@Autowired
+	public ISalesMngService iSalesMngService;
+	
 }
