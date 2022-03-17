268a1773cac7161e399b6df9ce5d50ffc0c831f1
영업_김민재 <poodle7192@gmail.com>
2022-03-17 14:58:54 +0900

    컨트롤러 수정

--- KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesController.java
@@ -1,5 +1,12 @@
 package com.gdj43.kberp.web.sales.controller;
 
-public class SalesController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.gdj43.kberp.web.common.service.ICommonService;
+
+@Controller
+public class SalesController {
+	@Autowired
+	public ICommonService iCommonService;
 }
