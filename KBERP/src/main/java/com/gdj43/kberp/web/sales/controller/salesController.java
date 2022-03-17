8c355e938c1ada9d5b3aef169fee2e528a96c27e
영업_김정효 <kjhyo96@naver.com>
2022-03-17 14:13:38 +0900

    salesController 수정

--- KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/salesController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/salesController.java
@@ -1,8 +1,12 @@
 package com.gdj43.kberp.web.sales.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.gdj43.kberp.web.common.service.ICommonService;
 
 @Controller
 public class salesController {
-
+	@Autowired
+	public ICommonService iCommonService;
 }
