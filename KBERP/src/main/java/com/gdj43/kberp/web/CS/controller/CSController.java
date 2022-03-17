3aad3751cbfbb1d34ec99e2290cf29cc20873f00
고객지원_조연지 <gdduswl96@gmail.com>
2022-03-17 15:36:28 +0900

    충돌해결

--- KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CSController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CSController.java
@@ -1,16 +1,8 @@
 package com.gdj43.kberp.web.CS.controller;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CSController {
-
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
-=======
-public class CSController {
->>>>>>> branch 'main' of https://github.com/axia911/gdj43.git
 
 }
