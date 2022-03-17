1c925e142dab0ade80b004286bdae3d71ebc9436
고객지원_김민호 <rla789753@naver.com>
2022-03-17 17:34:32 +0900

    컨트롤러 기본설정

--- KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CmbnController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CmbnController.java
@@ -1,8 +1,16 @@
 package com.gdj43.kberp.web.CS.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.gdj43.kberp.common.service.IPagingService;
+import com.gdj43.kberp.web.CS.service.ICmbnService;
 
 @Controller
 public class CmbnController {
-
+	@Autowired
+	public ICmbnService ics;
+	
+	@Autowired
+	public IPagingService ips;
 }
--- KBERP/src/main/java/com/gdj43/kberp/web/CS/service/CmbnService.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/CS/service/CmbnService.java
@@ -1,8 +1,12 @@
 package com.gdj43.kberp.web.CS.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gdj43.kberp.web.CS.dao.ICmbnDao;
 
 @Service
 public class CmbnService implements ICmbnService{
-
+	@Autowired
+	public ICmbnDao icd;
 }
