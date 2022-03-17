37958f1e8c99404792131d1d0a02d1783e340fe6
영업_김민재 <poodle7192@gmail.com>
2022-03-17 14:47:57 +0900

    sales 패키지 수정

--- KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/sales/controller/SalesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.gdj43.kberp.web.common.service.ICommonService;
 
 @Controller
-public class salesController {
+public class SalesController {
 	@Autowired
 	public ICommonService iCommonService;
 	
