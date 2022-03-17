250e9457e445539fcb5d0fcd82bd292c95a6f027
김정희 <jhk7951@gmail.com>
2022-03-17 15:09:06 +0900

    인사 java 기본 셋팅

--- KBERP/src/main/java/com/gdj43/kberp/web/hr/dao/HRDao.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/hr/dao/HRDao.java
@@ -1,5 +1,11 @@
 package com.gdj43.kberp.web.hr.dao;
 
-public class HRDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class HRDao implements IHRDao {
+	@Autowired
+	public SqlSession sqlSession;
 }
