7a512620e4701f15224e587a0a5ac6fc014d1c19
고객지원_권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2022-03-18 11:43:44 +0900

    CS 컨트롤러 / JSP 추가

--- KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/EventController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/EventController.java
@@ -1,8 +1,35 @@
 package com.gdj43.kberp.web.CS.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gdj43.kberp.common.service.IPagingService;
+import com.gdj43.kberp.web.CS.service.IEventService;
 
 @Controller
 public class EventController {
-
+	@Autowired
+	public IEventService iEventService;
+	
+	@Autowired
+	public IPagingService iPagingService;
+	
+	@RequestMapping(value="/prgrsEvent")
+	public ModelAndView prgrsEvent(ModelAndView mav) {
+		
+		mav.setViewName("CS/prgrsEvent");
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="/endEvent")
+	public ModelAndView endEvent(ModelAndView mav) {
+		
+		mav.setViewName("CS/endEvent");
+		
+		return mav;
+	}
+	
 }
