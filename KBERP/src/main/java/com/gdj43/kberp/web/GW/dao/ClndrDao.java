6aa2245957f8de65214046ed5ab2b4943c164506
정윤수 <bysizz1@naver.com>
2022-03-21 16:29:34 +0900

    일정등록기능 추가

--- KBERP/src/main/java/com/gdj43/kberp/web/GW/dao/ClndrDao.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/GW/dao/ClndrDao.java
@@ -1,8 +1,29 @@
 package com.gdj43.kberp.web.GW.dao;
 
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClndrDao implements IClndrDao{
+	@Autowired
+	public SqlSession sqlSession;
+
+	@Override
+	public void clndrAdd(HashMap<String, String> params) throws Throwable {
+		sqlSession.insert("clndr.clndrAdd", params);
+	}
+
+	@Override
+	public void clndrUpdate(HashMap<String, String> params) throws Throwable {
+		sqlSession.update("clndr.clndrUpdate", params);
+	}
+
+	@Override
+	public void clndrDelete(HashMap<String, String> params) throws Throwable {
+		sqlSession.update("clndr.clndrDelete", params);
+	}
 
 }
--- KBERP/src/main/java/com/gdj43/kberp/web/GW/service/ClndrService.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/GW/service/ClndrService.java
@@ -1,8 +1,32 @@
 package com.gdj43.kberp.web.GW.service;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gdj43.kberp.web.GW.dao.IClndrDao;
+
 
 @Service
 public class ClndrService implements IClndrService{
+	@Autowired
+	public IClndrDao iClndrDao;
+	
+	@Override
+	public void clndrAdd(HashMap<String, String> params) throws Throwable {
+		iClndrDao.clndrAdd(params);
+	}
+
+	@Override
+	public void clndrUpdate(HashMap<String, String> params) throws Throwable {
+		iClndrDao.clndrUpdate(params);
+	}
+
+	@Override
+	public void clndrDelete(HashMap<String, String> params) throws Throwable {
+		iClndrDao.clndrDelete(params);
+	}
+
 
 }
