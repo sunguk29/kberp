b7e96ac7bf9866bca1cb2b3b1bbd0b6bec08c0fe
Gwakdahun <ekgns7459@daum.net>
2022-03-22 18:15:16 +0900

    msgr Controller, Dao, Service추가

--- KBERP/src/main/java/com/gdj43/kberp/web/GW/service/IMsgrService.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/GW/service/IMsgrService.java
@@ -1,5 +1,9 @@
 package com.gdj43.kberp.web.GW.service;
 
+import java.util.HashMap;
+
 public interface IMsgrService {
+
+	public void insertCont(HashMap<String, String> params) throws Throwable;
 
 }
