6e97048b093b5e895467e849f7fc02f2e112b3e0
고객지원_김민호 <rla789753@naver.com>
2022-03-22 14:38:01 +0900

    안내글 페이지 추가 service dao 삭제

--- KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CmbnController.java
+++ KBERP/src/main/java/com/gdj43/kberp/web/CS/controller/CmbnController.java
@@ -13,25 +13,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdj43.kberp.common.bean.PagingBean;
 import com.gdj43.kberp.common.service.IPagingService;
-import com.gdj43.kberp.web.CS.service.ICmbnService;
+import com.gdj43.kberp.web.common.service.ICommonService;
+
+
 
 
 @Controller
 public class CmbnController {
+
 	@Autowired
-	public ICmbnService ics;
+	public ICommonService iCommonService;
 	
 	@Autowired
 	public IPagingService ips;
-	
-	@RequestMapping(value="guideWrtng")
-	public ModelAndView guideWrtng(ModelAndView mav) {
-		
-		mav.setViewName("CS/guideWrtng");
-		
-		return mav;
-	}
 	
 	@RequestMapping(value="strgBox")
 	public ModelAndView strgBox(ModelAndView mav) {
@@ -47,6 +43,20 @@ public class CmbnController {
 		return mav;
 	}
 	
+	@RequestMapping(value="/guideWrtng")
+	public ModelAndView guideWrtng(@RequestParam HashMap<String,String> params,
+									ModelAndView mav) {
+		
+		if(params.get("page")==null || params.get("page")==""){
+			params.put("page" , "1");
+		}
+		
+		mav.addObject("page",params.get("page"));
+		mav.setViewName("CS/guideWrtng");
+		
+		return mav;
+	}
+	
 	@RequestMapping(value="/guideWrtngAjax", method=RequestMethod.POST, 
 			produces="text/json;charset=UTF-8")
 	@ResponseBody // View로 인식 시킴
@@ -58,9 +68,18 @@ public class CmbnController {
 		Map<String,Object> modelMap = new HashMap<String,Object>();
 		
 		//구현 내용
-		List<HashMap<String,String>> list = ics.getGuide(params);
+		//총 게시글 수
+		int cnt= iCommonService.getIntData("cm.getGuideCnt",params);
+		//페이징 계산
+		PagingBean pb = ips.getPagingBean(Integer.parseInt(params.get("page")), cnt,3,5);
+		
+		params.put("startCount", Integer.toString(pb.getStartCount()));
+		params.put("endCount", Integer.toString(pb.getEndCount()));
+		
+		List<HashMap<String,String>> list = iCommonService.getDataList("cm.getGuide",params);
 		
 		modelMap.put("list",list);
+		modelMap.put("pb",pb);
 		
 		return mapper.writeValueAsString(modelMap); 
 	}
