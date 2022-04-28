d47d3bcb1cc65b5a5ed14900a8d767e07e366ce4
영업_유은정 <eundang41@naver.com>
2022-04-28 17:14:17 +0900

    다오,서비스 삭제

--- KBERP/src/main/java/com/gdj43/kberp/web/sales/dao/IClntRprtDao.java
+++
@@ -1,9 +0,0 @@
-package com.gdj43.kberp.web.sales.dao;
-
-import java.util.HashMap;
-
-public interface IClntRprtDao {
-
-	public HashMap<String, Object> getData(String string, HashMap<String, Object> params) throws Throwable;
-
-}
